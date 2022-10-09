55cf4c6f4c00a1d87712930a40f26840e8b46171
canon20paul <canon20paul@gmail.com>
2022-10-09 04:33:35 +0300

    Initial Files setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const board = [
+    {names:'Paul Malongo',scors:10},
+    {names:'Paul Malongo',scors:10},
+    {names:'Paul Malongo',scors:10},
+    {names:'Paul Malongo',scors:10},
+    {names:'Paul Malongo',scors:10},
+    {names:'Paul Malongo',scors:10}
+]
