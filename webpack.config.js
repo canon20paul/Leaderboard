5c345721055d4f3b9707362ebcbbe85834c1d9a5
canon20paul <canon20paul@gmail.com>
2022-10-14 14:32:16 +0300

    live updates

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
